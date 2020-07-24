ace9412a46b025bf39fa67d34926faf5fd038a08
shuimuyangsha <1023178387@qq.com>
2020-07-24 21:17:47 +0800

    学习class 和struct 的区别，class中的成员默认属性是私有。

--- Chapter10_Object_Class/main.cpp
+++ Chapter10_Object_Class/main.cpp
@@ -37,45 +37,64 @@ using namespace std;
 //}
 
 /*设计一个学生类，属性有姓名和学号，可以给姓名和学号赋值，可以显示学生的姓名和学号*/
-class Student
-{
-public://公共权限
-	int m_ID;//学号
-private:
-	//属性
+//class Student
+//{
+//public://公共权限
+//	int m_ID;//学号
+//private:
+//	//属性
+//
+//	
+//	string m_Name;//姓名
+//
+//	//行为
+//public:
+//	void showStudent()
+//	{
+//		cout << "姓名：" << m_Name << " 学号：" << m_ID << endl;
+//	}
+//	//给姓名赋值
+//	void setName(string name)
+//	{
+//		m_Name = name;
+//	}
+//
+//};
+//int main()
+//{
+//	Student stu1;
+//	stu1.setName("董晓辉");
+//	stu1.m_ID = 1382332;
+//
+//	stu1.showStudent();
+//
+//	Student stu2;
+//	stu2.setName("诸葛亮");
+//	stu2.m_ID = 2;
+//
+//	stu2.showStudent();
+//	system("pause");
+//	return 0;
+//}
 
-	
-	string m_Name;//姓名
-
-	//行为
-public:
-	void showStudent()
-	{
-		cout << "姓名：" << m_Name << " 学号：" << m_ID << endl;
-	}
-	//给姓名赋值
-	void setName(string name)
-	{
-		m_Name = name;
-	}
+/* struct与 class 的区别,class 中成员的默认属性是私有*/
+class C1 {
+	int m_A;
 
 };
-int main()
+struct C2 {
+	int m_B;
+};
+
+int main(void)
 {
-	Student stu1;
-	stu1.setName("董晓辉");
-	stu1.m_ID = 1382332;
+	C1 c1;
+	C2 c2;
 
-	stu1.showStudent();
+	//c1.m_A = 10;//不可以访问类中的成员，因为它是私有的
+	c2.m_B = 10;//可以正常访问类中的成员。
 
-	Student stu2;
-	stu2.setName("诸葛亮");
-	stu2.m_ID = 2;
-
-	stu2.showStudent();
 	system("pause");
 	return 0;
 }
-
-
 
